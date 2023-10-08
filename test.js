a54029550f712cbe6093f2d07558e18b01998c6b
Prabin\clear <bajgaiprabin@gmail.com>
2023-10-08 18:44:31 -0500

    More work on Singly Linked List

--- test.js
+++ test.js
@@ -14,6 +14,8 @@ class SinglyLinkedList {
     this.head = null;
     this.tail = null;
   }
+
+  //Add to the beginning of the Linked List
   push(val) {
     let newNode = new Node(val);
     if (!this.head) {
@@ -26,10 +28,61 @@ class SinglyLinkedList {
     this.length++;
     return this;
   }
+
+  //Remove and return the last item of the Linked List
+  pop() {
+    if (!this.head) return undefined;
+
+    let current = this.head;
+    let newTail = current;
+
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+
+    if (this.length === 0) {
+      // If the list becomes empty after popping, update the head as well
+      this.head = null;
+    }
+    return current.val;
+  }
+
+  //Remove and return the first node from the Linked List
+  shift() {
+    if (!this.head) return undefined;
+    let currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+
+    if (this.length === 0) {
+      // If the list becomes empty after popping, update the head as well
+      this.head = null;
+      this.tail = null;
+    }
+    return currentHead.val;
+  }
+
+  unshift(val) {
+    let newHead = new Node(val);
+    if (!this.head) {
+      this.head = newHead;
+      this.tail = newHead;
+    } else {
+      newHead.next = this.head;
+      this.head = newHead;
+      this.length++;
+    }
+    return this;
+  }
 }
 
 let list = new SinglyLinkedList();
-list.push("Hi");
 list.push("There");
 list.push("Stranger");
-console.log(list);
+
+console.log(list.unshift("Ahoy"));
