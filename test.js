56c036ede8d130b788d5d57bf261098d90d5d0cd
Prabin\clear <bajgaiprabin@gmail.com>
2023-10-08 14:39:23 -0500

    Initial file. Working on Singly Linked List

---
+++ test.js
@@ -0,0 +1,35 @@
+//piece of data - val
+//reference to the next node- next
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.length = 0;
+    this.head = null;
+    this.tail = null;
+  }
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+let list = new SinglyLinkedList();
+list.push("Hi");
+list.push("There");
+list.push("Stranger");
+console.log(list);
