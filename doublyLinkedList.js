b898c013db7147868ff5dd14a53c75ed2ce37b4f
Prabin\clear <bajgaiprabin@gmail.com>
2023-10-17 00:40:41 -0500

    Worked on Doubly Linked List

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -25,6 +25,72 @@ class DoublyLinkedList {
     this.length++;
     return this;
   }
+
+  pop() {
+    if (this.head === null) return undefined;
+
+    let temp = this.tail;
+    if (this.length === 1) {
+      this.head = this.tail = null;
+    } else {
+      this.tail = temp.prev;
+      this.tail.next = null;
+      temp.prev = null;
+      console.log(this);
+    }
+    this.length--;
+    return temp;
+  }
+  //Shift- remove a node from the beginning
+
+  shift() {
+    if (!this.head) return undefined;
+    let temp = this.head;
+    if (this.length === 1) {
+      this.head = this.tail = null;
+    } else {
+      this.head = temp.next;
+      this.head.prev = null;
+      temp.next = null;
+    }
+    this.length--;
+    return temp;
+  }
+  unshift(val) {
+    let newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+
+    if (index <= this.length / 2) {
+      let count = 0;
+      let current = this.head;
+      while (index != count) {
+        current = current.next;
+        count++;
+      }
+      return current;
+    } else {
+      let count = this.length - 1;
+      let current = this.tail;
+      while (index != count) {
+        current = current.prev;
+        count--;
+      }
+      return current;
+    }
+  }
 }
 
 const list = new DoublyLinkedList();
@@ -33,4 +99,7 @@ list.push(1);
 list.push(3);
 list.push(6);
 list.push(1000);
-console.log(list.push(2));
+list.push(20);
+list.push(49);
+
+console.log(list.get(4));
