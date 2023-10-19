647eb60c4e3cb8b750e4e34f35e6ef564c2b7ed8
Prabin\clear <bajgaiprabin@gmail.com>
2023-10-18 23:37:37 -0500

    Finished Stacks and Queues

---
+++ stacksAndQueues.js
@@ -0,0 +1,76 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  push(val) {
+    let newNode = new Node(val);
+    if (this.size === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      let temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+    return ++this.size;
+  }
+
+  pop() {
+    if (this.size === 0) return null;
+    let temp = this.first;
+
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.size--;
+    this.first = this.first.next;
+
+    return temp.value;
+  }
+}
+
+let stack = new Stack();
+stack.push(1);
+stack.push(342);
+stack.push(2);
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  enqueue(val) {
+    var newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    return ++this.size;
+  }
+
+  dequeue() {
+    if (!this.first) return null;
+
+    var temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+}
